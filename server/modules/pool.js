90d833a22d2c1e0f7025f8ceb8e893b1597b48cd
AwrenNuit <a.rennaker@yahoo.com>
2019-12-15 09:53:57 -0600

    set up for Heroku deployment

--- server/modules/pool.js
+++ server/modules/pool.js
@@ -1,16 +1,8 @@
-/**
-* You'll need to use environment variables in order to deploy your
-* pg-pool configuration to Heroku.
-* It will look something like this:
-**/
-
 const pg = require('pg');
 const url = require('url');
 let config = {};
 
 if (process.env.DATABASE_URL) {
-    // Heroku gives a url, not a connection object
-    // https://github.com/brianc/node-pg-pool
     let params = url.parse(process.env.DATABASE_URL);
     let auth = params.auth.split(':');
 
@@ -25,7 +17,8 @@ if (process.env.DATABASE_URL) {
         idleTimeoutMillis: 30000, // how long a client is allowed to remain idle before being closed
     };
 
-} else {
+}
+else {
     // only change the things on the right side of the ||
     config = {
         user: process.env.PG_USER || null, //env var: PGUSER
