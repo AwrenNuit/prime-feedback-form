73794b0aaa6b5d04e3d792b6c5fa95f5ad0dfc99
Awren Nuit <54560441+AwrenNuit@users.noreply.github.com>
2020-01-08 08:19:29 -0600

    updated formatting

--- server/routes/route.js
+++ server/routes/route.js
@@ -3,7 +3,6 @@ const router = express.Router();
 const pool = require('../modules/pool');
 
 router.delete(`/delete/:id`, (req, res) => {
-  console.log('in DELETE');
   let id = [req.params.id];
   let SQLquery = `DELETE FROM feedback WHERE id = $1;`;
   pool.query(SQLquery, id)
@@ -14,35 +13,32 @@ router.delete(`/delete/:id`, (req, res) => {
     console.log('ERROR IN DELETE -------------------------------->', error)
     res.sendStatus(500);
   });
-})
+});
 
 router.get(`/`, (req, res) => {
-  console.log('in GET');
   pool.query(`SELECT * FROM feedback ORDER BY date ASC, id ASC;`)
   .then((result)=>{
     res.send(result.rows);
   })
   .catch((error)=>{
   console.log('ERROR IN GET ------------------------------->', error);
-  })
-})
+  });
+});
 
 router.post('/', (req, res) => {
-    console.log('in POST');
-    let id = [req.body.feeling, req.body.understanding, req.body.support, req.body.comments];
-    let SQLquery = `INSERT INTO feedback ("feeling", "understanding", "support", "comments") VALUES($1, $2, $3, $4);`;
-    pool.query(SQLquery, id)
-    .then((result) => {
-      res.send(201);
-    })
-    .catch((error) => {
-      console.log('ERROR IN POST -------------------------------->', error)
-      res.sendStatus(500);
-    });
-})
+  let id = [req.body.feeling, req.body.understanding, req.body.support, req.body.comments];
+  let SQLquery = `INSERT INTO feedback ("feeling", "understanding", "support", "comments") VALUES($1, $2, $3, $4);`;
+  pool.query(SQLquery, id)
+  .then((result) => {
+    res.send(201);
+  })
+  .catch((error) => {
+    console.log('ERROR IN POST -------------------------------->', error)
+    res.sendStatus(500);
+  });
+});
 
 router.put(`/flag/:id`, (req, res) => {
-  console.log('in PUT');
   let id = [req.body.flag, req.params.id];
   let SQLquery = `UPDATE feedback SET flagged = $1 WHERE id = $2;`;
   pool.query(SQLquery, id)
@@ -53,6 +49,6 @@ router.put(`/flag/:id`, (req, res) => {
     console.log('ERROR IN PUT -------------------------------->', error)
     res.sendStatus(500);
   });
-})
+});
 
 module.exports = router;
